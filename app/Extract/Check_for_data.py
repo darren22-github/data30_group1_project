f933b01fbcc790e7487dad0986c02fc6aca9dd9f
Pixanu <quikyboy2@gmail.com>
2022-07-27 16:53:16 +0100

    extract Txt files (Sparta Day ) & Academy + Unit Testing

--- app/Extract/Check_for_data.py
+++ app/Extract/Check_for_data.py
@@ -13,18 +13,7 @@ def list_bucket():
         object_keys.append(i.key)
 
     return object_keys
-    # List objects in a bucket given a path, returns each key in a list
 
-    # AWS_BUCKET_PREFIX = 'Talent/Sparta'
-    # bucket_contents = s3_client.list_objects(Bucket=bucket_name)
-    # for file in bucket_contents["Contents"]:
-    #     file_name = file["Key"]
-    #     pprint(file_name)
-    #
-    # obj = s3_client.get_object(Bucket=bucket_name, Key=file_name)
-    # body = obj["Body"]
-    # acad = pd.read_table(body)
-    # pprint(acad)
 
 def list_talents(prefix = ''):
     list_talents =[]
@@ -33,7 +22,6 @@ def list_talents(prefix = ''):
     talents = filter(lambda x: x.endswith('.json'), object_keys)
     for obj in talents:
         list_talents.append(obj)
-
     # list_talents_extended = []
     # [list_talents_extended.extend(i) for i in list_talents]
 
@@ -62,6 +50,18 @@ def list_applicants():
     pprint(applicant_list)
     return applicant_list
 
+def list_courses():
+    courses_list=[]
+    #courses = ['Business','Data','Engineering']
+
+    object_keys = list_bucket()
+    c_list = filter(lambda x: x.startswith('Academy/'), object_keys)
+    for obj in c_list:
+        courses_list.append(obj)
+    pprint(courses_list)
+    return courses_list
+
+
 
 # EXTRACT OBJECTS
 def extract_object(key):
@@ -79,6 +79,7 @@ def extract_all_talent():
     for t in list_talents():
         extract_talent(t)
 
+
 # EXTRACT APPLICANT
 def extract_applicant(key):
     obj = extract_object(key)
@@ -91,6 +92,31 @@ def extract_all_applicants():
     for a in list_applicants():
         print(extract_applicant(a))
 
+
+# EXTRACT SPARTA DAY
+def extract_sparta_day(key):
+    obj = extract_object(key)
+    obj_sparta_day= []
+    #obj = obj['Body'].read()
+    for line in obj['Body'].iter_lines():
+            obj_sparta_day.append(line.decode('utf-8'))
+
+    return obj_sparta_day
+
+def extract_all_sparta_day():
+    for a in list_spartaday():
+        print(extract_sparta_day(a))
+
+#EXTRACT ACADEMY
+def extract_course(key):
+    obj = extract_object(key)
+    df = pd.read_csv(obj['Body'])
+
+    return df
+
+def extract_all_courses():
+    for a in list_courses():
+        pprint(extract_course(a))
 
 def difference_files(list1, list2):
     # Get the difference between the two lists provided
@@ -112,6 +138,14 @@ def difference_files(list1, list2):
 #extract_talent('Talent/10445.json')
 #extract_all_talent()
 #extract_all_applicants()
+
+#list_courses()
+#pprint(extract_course('Academy/Business_20_2019-02-11.csv'))
+#extract_all_sparta_day()
+#extract_all_courses()
+print(len(list_bucket()))
+print(len(list_talents()))
+print(len(list_spartaday()))
 
 
 
--- app/Tests/test_read_all_csv.py
+++ app/Tests/test_read_all_csv.py
@@ -1,14 +1,10 @@
 import boto3
 from pprint import pprint
 import pandas as pd
+# from app.Extract.s3_client import *
+# from app.Extract.Check_for_data import list_bucket
 
-def test_get_all_csv():
-    s3_client = boto3.client('s3')
-    bucket_name = 'data-eng-30-final-project-files'
 
-    academy_file = s3_client.list_objects(Bucket=bucket_name, Prefix='Academy/')['Contents']
-
-    pprint(academy_file)
 
 
 def test_get_all_files():
@@ -46,8 +42,40 @@ def test_s3_connection():
 
     assert status == 200
 
+#----------------------------------------------------------------
+#Final Testing
 
-test_get_all_files()
-test_get_all_csv()
-test_read_csv()
-test_s3_connection()
+def test_list_bucket():
+    s3_client = boto3.client('s3')
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket('data-eng-30-final-project-files')
+    object_keys = []
+
+    for i in bucket.objects.all():
+        object_keys.append(i.key)
+
+    assert len(object_keys) == 3305
+    return object_keys
+
+def test_list_talents():
+    list_talents = []
+
+    object_keys = test_list_bucket()
+    talents = filter(lambda x: x.endswith('.json'), object_keys)
+    for obj in talents:
+        list_talents.append(obj)
+
+    assert len(list_talents) ==3105
+    return list_talents
+
+def test_list_spartaday():
+    list_spartaday = []
+
+    object_keys = test_list_bucket()
+    spartaDay = filter(lambda x: x.endswith('.txt'), object_keys)
+    for obj in spartaDay:
+        list_spartaday.append(obj)
+
+    assert list_spartaday[1] == 'Talent/Sparta Day 1 May 2019.txt'
+    assert len(list_spartaday) == 152
+    return list_spartaday
