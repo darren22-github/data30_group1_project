ff2473693d30db91d6431c0956f4446ca5840ffd
dg0835 <95049866+dg0835@users.noreply.github.com>
2022-07-27 15:16:33 +0100

    Added some unit tests for the dataCleaner class

--- test_transformation.py
+++ test_transformation.py
@@ -1,7 +1,24 @@
 import unittest
-import transformation
 
-class UnitTests(unittest.TestCase):
+from pandas import read_csv
+import transformation
+import csv
+
+test_rows = ['6','Joel Melmoth','Male','04/02/1994','jmelmoth5@dedecms.com','Manchester','2 Esker Center','M14','+44 860 146 8614','Napier University','02:01','','',]
+
+class TestDataCleaner(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.dc = transformation.DataCleaner()
+
+    def test_check_if_empty(self):
+        print('Testing check_if_empty method')
+        self.assertEqual(self.dc.check_if_empty(""), True)
+        self.assertEqual(self.dc.check_if_empty("hello"), False)
+
+    def test_empty_to_null(self):
+        print('Testing empty_to_null method')
+        self.assertEqual(self.dc.empty_to_null(""), "null")
+
+if __name__ == '__main__':
+    unittest.main()
